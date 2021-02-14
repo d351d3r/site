f0b052b76ad11e662f210bc447253a405bbf5f6a
Anatolii Cherednichenko <greatestcombinator@gmail.com>
2021-02-14 18:29:36 +1000

    Update site

--- gitupdate.sh
+++ gitupdate.sh
@@ -4,3 +4,8 @@ git add .
 git commit -m "Update site"
 git push origin master
 echo "Sources updated"
+echo "Update public"
+cd public
+git add .
+git commit -m "Pubic updated!"
+git push origin main
